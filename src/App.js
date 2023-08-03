ef3d9d9bf68fc8b93e59de55ab14ac55470754b3
Thaissa Carvalho <thaissa.dos2003@gmail.com>
2023-08-03 09:26:41 -0300

    Alura-finished Formulario-UI

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import "./App.css";
+import FormularioCadastro from "./Components/FormularioCadastro/FormularioCadastro";
+import {Container, Typography } from "@mui/material"
+
+class App extends Component {
+  render() {
+    return (
+      <Container component="article" maxWidth="sm">
+        <Typography variant="h3" component="h1" align="center" >Formulário de cadastro</Typography>
+        <FormularioCadastro aoEnviar={aoEnviarForm} validarCpf={validarCPF} />
+      </Container>
+    );
+  }
+}
+
+function aoEnviarForm(dados){
+  console.log(dados);
+}
+
+function validarCPF(cpf){
+  if(cpf.length !== 11){
+    return {valido:false, texto:"CPF deve ter 11 digitos."}
+  }else{
+    return {valido:true, texto:""}
+  }
+}
+
+export default App;
